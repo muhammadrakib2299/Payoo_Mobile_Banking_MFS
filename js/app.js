b57f09a963b23d8650603f209c51aec3f288a5ea
muhammadrakib2299 <muhammad.rakib2299@gmail.com>
2025-02-07 15:58:30 +0600

    get the input filed value given by user

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-console.log('hello from js file');
+// select login button 
+document.getElementById('btn-login').addEventListener('click', function(e){
+    e.preventDefault();
+    // get the phone number from the input field
+    const phoneNumber = document.getElementById('phone-number-filed').value;
+    // get the Pin number from the input filed
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(phoneNumber, pinNumber);
+
+    
+
+});
