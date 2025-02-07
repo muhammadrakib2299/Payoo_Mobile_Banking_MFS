4c990a0f8a57e6b288ccda99e88481e3da1fb46d
muhammadrakib2299 <muhammad.rakib2299@gmail.com>
2025-02-07 20:21:10 +0600

    succesfully work add money part and aslo display update amount

--- js/home.js
+++ js/home.js
@@ -1,1 +1,17 @@
-console.log("hello from home page")
+// add money process
+document.getElementById('btn-add-money').addEventListener('click', function(e){
+    e.preventDefault();    
+    // get the user input amount
+    const addAmount = document.getElementById('add-money-filed').value;
+    const pinCode = document.getElementById('pin-filed').value;
+   
+    if(pinCode === '1234'){
+        // current balance
+        const currentBalance = document.getElementById('avilable-balance').innerText;
+        const updateBalance = parseFloat(currentBalance) + parseFloat(addAmount);
+        document.getElementById('avilable-balance').innerText = updateBalance;
+    }
+    else{
+        alert("Wrong Pin code")
+    }
+});
