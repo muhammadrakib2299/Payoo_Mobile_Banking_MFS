dcb09113316cacf2d0a436f30074bcec886205f3
muhammadrakib2299 <muhammad.rakib2299@gmail.com>
2025-02-07 22:15:08 +0600

    added another part is cash out part, user successfully cash out their money

--- js/home.js
+++ js/home.js
@@ -15,3 +15,23 @@ document.getElementById('btn-add-money').addEventListener('click', function(e){
         alert("Wrong Pin code")
     }
 });
+
+// Cash Out Money Process
+document.getElementById('btn-cashout').addEventListener('click', function(e){
+    e.preventDefault();
+    // get the user input
+    
+    const cashoutAmount = document.getElementById('cashout-filed').value;
+    const cashoutPin = document.getElementById('cashoutPin-filed').value;
+
+    if(cashoutPin === '1234'){
+        const balance = document.getElementById('avilable-balance').innerText;
+        const afterCashoutBalance = parseFloat(balance) - parseFloat(cashoutAmount);
+
+        document.getElementById('avilable-balance').innerText = afterCashoutBalance;
+    }
+    else{
+        alert("Wrong Pin");
+    }
+
+});
