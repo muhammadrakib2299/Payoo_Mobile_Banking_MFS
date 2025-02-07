5111a1b182c7f95d31a5ff9e88e5859bf2713f8f
muhammadrakib2299 <muhammad.rakib2299@gmail.com>
2025-02-07 16:12:34 +0600

    add a home page, if any user authentication successfully done, after the user can go the page

--- js/app.js
+++ js/app.js
@@ -5,8 +5,15 @@ document.getElementById('btn-login').addEventListener('click', function(e){
     const phoneNumber = document.getElementById('phone-number-filed').value;
     // get the Pin number from the input filed
     const pinNumber = document.getElementById('pin-number').value;
-    console.log(phoneNumber, pinNumber);
-
-    
+   
+    // console.log(phoneNumber, pinNumber);
+    // Temporary user authenticon 
+    if(phoneNumber ==='01609' && pinNumber==='1234'){
+        console.log('you are login');
+        window.location.href = './home.html';
+    }
+    else{
+        alert('Wrong Phone Number or Pin');
+    }
 
 });
